83073755181d6573ad094d6cf2c4376108ff9bd1
Hans Westerbeek <hans@enhance-ict.com>
2015-06-23 10:03:42 +0200

    Initial

---
+++ src/main/java/demo/LogbackFailureDemoApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackFailureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackFailureDemoApplication.class, args);
+    }
+}
